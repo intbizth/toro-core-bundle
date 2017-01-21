512ef0ebb94f7413e02bf649e62f501c51d09bda
liverbool <nukboon@gmail.com>
2017-01-21 15:56:13 +0700

    add taxon options

--- Model/Taxon.php
+++ Model/Taxon.php
@@ -6,4 +6,24 @@ use Sylius\Component\Taxonomy\Model\Taxon as BaseTaxon;
 
 class Taxon extends BaseTaxon
 {
+    /**
+     * @var array
+     */
+    protected $options = [];
+
+    /**
+     * @return array
+     */
+    public function getOptions(): array
+    {
+        return $this->options;
+    }
+
+    /**
+     * @param array $options
+     */
+    public function setOptions(array $options)
+    {
+        $this->options = $options;
+    }
 }
