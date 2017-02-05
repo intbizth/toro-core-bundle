0ccc2058382ebeed276ee5b55e832d45ac3ef84f
liverbool <nukboon@gmail.com>
2017-02-05 21:49:22 +0700

    add missing form field

--- Form/Type/PostFlaggedTypeType.php
+++ Form/Type/PostFlaggedTypeType.php
@@ -4,6 +4,7 @@ namespace Toro\Bundle\CoreBundle\Form\Type;
 
 use Sylius\Bundle\ResourceBundle\Form\EventSubscriber\AddCodeFormSubscriber;
 use Sylius\Bundle\ResourceBundle\Form\Type\AbstractResourceType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Toro\Bundle\CmsBundle\Form\Type\YamlType;
@@ -19,6 +20,10 @@ class PostFlaggedTypeType extends AbstractResourceType
             ->add('name', TextType::class, [
                 'required' => true,
                 'label' => 'Name',
+            ])
+            ->add('singleActive', CheckboxType::class, [
+                'required' => false,
+                'label' => 'Single Active',
             ])
             ->add('config', YamlType::class, [
                 'required' => true,
