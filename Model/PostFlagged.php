8d0b3699e8c01ff0d844f768caa8fc6e33251ea0
liverbool <nukboon@gmail.com>
2017-03-24 04:55:34 +0700

    bugfix

--- Model/PostFlagged.php
+++ Model/PostFlagged.php
@@ -4,6 +4,7 @@ namespace Toro\Bundle\CoreBundle\Model;
 
 use Sylius\Component\Resource\Model\TimestampableTrait;
 use Toro\Bundle\CmsBundle\Model\Flagged;
+use Toro\Bundle\CmsBundle\Model\FlaggedAwareInterface;
 
 class PostFlagged extends Flagged implements PostFlaggedInterface
 {
@@ -26,5 +27,21 @@ class PostFlagged extends Flagged implements PostFlaggedInterface
     public function setPost(PostInterface $post = null)
     {
         $this->post = $post;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFlaggedAware()
+    {
+        return $this->post;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setFlaggedAware(FlaggedAwareInterface $flaggedAware = null)
+    {
+        $this->setPost($flaggedAware);
     }
 }
