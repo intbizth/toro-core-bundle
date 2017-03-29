30345a0c9433ea9426e116eac064ed0a034f271f
liverbool <nukboon@gmail.com>
2017-03-30 02:28:20 +0700

    update customer choice type & autocomplete

---
+++ Form/Type/CustomerChoiceType.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Toro\Bundle\CoreBundle\Form\Type;
+
+use Sylius\Component\Resource\Repository\RepositoryInterface;
+use Symfony\Bridge\Doctrine\Form\DataTransformer\CollectionToArrayTransformer;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\Options;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+final class CustomerChoiceType extends AbstractType
+{
+    /**
+     * @var RepositoryInterface
+     */
+    private $customerRepository;
+
+    /**
+     * @param RepositoryInterface $customerRepository
+     */
+    public function __construct(RepositoryInterface $customerRepository)
+    {
+        $this->customerRepository = $customerRepository;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'choices' => function (Options $options) {
+                return $this->customerRepository->findAll();
+            },
+            'choice_value' => 'email',
+            'choice_label' => 'name',
+            'choice_translation_domain' => false,
+            'class' => $this->customerRepository->getClassName(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+        return EntityType::class;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'toro_customer_choice';
+    }
+}
