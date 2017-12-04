3a6e5a461ff76505ca247c4ce7848c27cff14ae9
phakpoom <im_bonbonn@hotmail.com>
2017-12-04 14:29:05 +0700

    change method user update password

--- EventListener/PasswordUpdaterListener.php
+++ EventListener/PasswordUpdaterListener.php
@@ -24,7 +24,7 @@ final class PasswordUpdaterListener extends BasePasswordUpdaterListener
         }
 
         if (null !== $user = $customer->getUser()) {
-            $this->updateUserPassword($user);
+            $this->updatePassword($user);
         }
     }
 }
