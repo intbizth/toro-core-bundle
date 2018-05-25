d7352d63343c4ac9a2ef8aa1576d94dd9d69d2ce
Phakpoom V <im_bonbonn@hotmail.com>
2018-05-25 12:12:56 +0700

    Update Taxon.php

--- Model/Taxon.php
+++ Model/Taxon.php
@@ -6,7 +6,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Sylius\Component\Taxonomy\Model\Taxon as BaseTaxon;
 use Sylius\Component\Taxonomy\Model\TaxonInterface;
-use Sylius\Component\Taxonomy\Model\TaxonTranslationInterface;
+use Sylius\Component\Taxonomy\Model\TaxonTranslation as SyliusTaxonTranslation;
 
 class Taxon extends BaseTaxon
 {
@@ -55,7 +55,7 @@ class Taxon extends BaseTaxon
      * {@inheritdoc}
      * {@internal}
      */
-    public function createTranslation(): TaxonTranslationInterface
+    public function createTranslation(): SyliusTaxonTranslation
     {
         return new TaxonTranslation();
     }
