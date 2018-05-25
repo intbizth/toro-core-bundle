5f22ee82d058d04a60bda3a10547cbbc7dedddd5
Phakpoom V <im_bonbonn@hotmail.com>
2018-05-25 13:46:52 +0700

    Update WebUserInterface.php

--- Model/WebUserInterface.php
+++ Model/WebUserInterface.php
@@ -4,7 +4,8 @@ namespace Toro\Bundle\CoreBundle\Model;
 
 use Sylius\Component\Customer\Model\CustomerAwareInterface;
 use Symfony\Component\Security\Core\User\EquatableInterface;
+use Dos\Bundle\OAuthServerBundle\Model\UserInterface as DosOauthUserInterface;
 
-interface WebUserInterface extends UserInterface, CustomerAwareInterface, EquatableInterface
+interface WebUserInterface extends UserInterface, CustomerAwareInterface, EquatableInterface, DosOauthUserInterface
 {
 }
